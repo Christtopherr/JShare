28b1d4735c41d94fe28f93ff63f0ecf84605fe2a
Christopher Gomes <christopher@certto.com.br>
2016-05-04 19:47:59 -0300

    Seguindo o Projeto

--- JShare135208/src/br/univel/pojos/Diretorio.java
+++ JShare135208/src/br/univel/pojos/Diretorio.java
@@ -2,4 +2,14 @@ package br.univel.pojos;
 
 public class Diretorio {
 
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
 }
--- JShare135208/src/br/univel/testes/Testar.java
+++ JShare135208/src/br/univel/testes/Testar.java
@@ -1,5 +1,37 @@
 package br.univel.testes;
 
+import java.awt.EventQueue;
+
+import br.univel.cliente.FrameCliente;
+import br.univel.servidor.FrameServidor;
+
 public class Testar {
 
+	public static void main(String[] args) {
+
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+
+					FrameServidor servidor = new FrameServidor();
+					servidor.setVisible(true);
+					servidor.configurar();
+					
+					servidor.setBounds(0, 0, 1000, 300);
+					
+					FrameCliente cliente = new FrameCliente();
+					cliente.setVisible(true);
+					cliente.configurar();
+					cliente.setBounds(0, 350, 1000, 300);
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+	}
+
 }
+
+
