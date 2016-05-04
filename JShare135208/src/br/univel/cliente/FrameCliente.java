69e23a9ee74bbbc50cb534974fb7222c76643c33
Christopher Gomes <christopher@certto.com.br>
2016-05-03 22:27:35 -0300

    Comit CLiente

--- JShare135208/src/br/univel/cliente/FrameCliente.java
+++ JShare135208/src/br/univel/cliente/FrameCliente.java
@@ -1,5 +1,553 @@
 package br.univel.cliente;
 
-public class FrameCliente {
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+import br.univel.interfaces.Cliente;
+import br.univel.interfaces.IServer;
+import br.univel.pojos.Arquivo;
+
+public class FrameCliente extends JFrame implements IServer {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrameCliente frame = new FrameCliente();
+					frame.setVisible(true);
+					frame.configurar();
+					frame.setBounds(0, 350, 800, 300);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FrameCliente() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 481, 409);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[] { 48, 162, 34, 65, 82, 0 };
+		gbl_contentPane.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		gbl_contentPane.columnWeights = new double[] { 0.0, 1.0, 0.0, 1.0, 0.0, Double.MIN_VALUE };
+		gbl_contentPane.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE };
+		contentPane.setLayout(gbl_contentPane);
+
+		JLabel lblCliente = new JLabel("JShare");
+		lblCliente.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		GridBagConstraints gbc_lblCliente = new GridBagConstraints();
+		gbc_lblCliente.gridwidth = 5;
+		gbc_lblCliente.insets = new Insets(0, 0, 5, 0);
+		gbc_lblCliente.gridx = 0;
+		gbc_lblCliente.gridy = 0;
+		contentPane.add(lblCliente, gbc_lblCliente);
+
+		JLabel lblNome = new JLabel("Nome");
+		GridBagConstraints gbc_lblNome = new GridBagConstraints();
+		gbc_lblNome.anchor = GridBagConstraints.EAST;
+		gbc_lblNome.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNome.gridx = 0;
+		gbc_lblNome.gridy = 1;
+		contentPane.add(lblNome, gbc_lblNome);
+
+		txtNome = new JTextField();
+		txtNome.setText("Juan Pablo");
+		GridBagConstraints gbc_txtNome = new GridBagConstraints();
+		gbc_txtNome.insets = new Insets(0, 0, 5, 5);
+		gbc_txtNome.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtNome.gridx = 1;
+		gbc_txtNome.gridy = 1;
+		contentPane.add(txtNome, gbc_txtNome);
+		txtNome.setColumns(10);
+
+		JLabel lblIp = new JLabel("IP Servidor");
+		GridBagConstraints gbc_lblIp = new GridBagConstraints();
+		gbc_lblIp.anchor = GridBagConstraints.EAST;
+		gbc_lblIp.insets = new Insets(0, 0, 5, 5);
+		gbc_lblIp.gridx = 0;
+		gbc_lblIp.gridy = 2;
+		contentPane.add(lblIp, gbc_lblIp);
+
+		txtIP = new JTextField();
+		txtIP.setText("127.0.0.1");
+		GridBagConstraints gbc_txtIP = new GridBagConstraints();
+		gbc_txtIP.insets = new Insets(0, 0, 5, 5);
+		gbc_txtIP.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtIP.gridx = 1;
+		gbc_txtIP.gridy = 2;
+		contentPane.add(txtIP, gbc_txtIP);
+		txtIP.setColumns(10);
+
+		JLabel lblPorta = new JLabel("Porta");
+		GridBagConstraints gbc_lblPorta = new GridBagConstraints();
+		gbc_lblPorta.anchor = GridBagConstraints.EAST;
+		gbc_lblPorta.insets = new Insets(0, 0, 5, 5);
+		gbc_lblPorta.gridx = 2;
+		gbc_lblPorta.gridy = 2;
+		contentPane.add(lblPorta, gbc_lblPorta);
+
+		txtPorta = new JTextField();
+		txtPorta.setText("1818");
+		txtPorta.setColumns(10);
+		GridBagConstraints gbc_txtPorta = new GridBagConstraints();
+		gbc_txtPorta.insets = new Insets(0, 0, 5, 5);
+		gbc_txtPorta.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtPorta.gridx = 3;
+		gbc_txtPorta.gridy = 2;
+		contentPane.add(txtPorta, gbc_txtPorta);
+
+		lblSeuIp = new JLabel("Seu IP");
+		GridBagConstraints gbc_lblSeuIp = new GridBagConstraints();
+		gbc_lblSeuIp.anchor = GridBagConstraints.EAST;
+		gbc_lblSeuIp.insets = new Insets(0, 0, 5, 5);
+		gbc_lblSeuIp.gridx = 0;
+		gbc_lblSeuIp.gridy = 3;
+		contentPane.add(lblSeuIp, gbc_lblSeuIp);
+
+		txtMyIP = new JTextField();
+		txtMyIP.setText("192.168.65.1");
+		txtMyIP.setColumns(10);
+		GridBagConstraints gbc_txtMyIP = new GridBagConstraints();
+		gbc_txtMyIP.insets = new Insets(0, 0, 5, 5);
+		gbc_txtMyIP.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtMyIP.gridx = 1;
+		gbc_txtMyIP.gridy = 3;
+		contentPane.add(txtMyIP, gbc_txtMyIP);
+
+		label_1 = new JLabel("Porta");
+		GridBagConstraints gbc_label_1 = new GridBagConstraints();
+		gbc_label_1.anchor = GridBagConstraints.EAST;
+		gbc_label_1.insets = new Insets(0, 0, 5, 5);
+		gbc_label_1.gridx = 2;
+		gbc_label_1.gridy = 3;
+		contentPane.add(label_1, gbc_label_1);
+
+		txtMyPort = new JTextField();
+		txtMyPort.setText("1819");
+		txtMyPort.setColumns(10);
+		GridBagConstraints gbc_txtMyPort = new GridBagConstraints();
+		gbc_txtMyPort.insets = new Insets(0, 0, 5, 5);
+		gbc_txtMyPort.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtMyPort.gridx = 3;
+		gbc_txtMyPort.gridy = 3;
+		contentPane.add(txtMyPort, gbc_txtMyPort);
+
+		lblNomeDoArquivo = new JLabel("Nome do Arquivo");
+		GridBagConstraints gbc_lblNomeDoArquivo = new GridBagConstraints();
+		gbc_lblNomeDoArquivo.anchor = GridBagConstraints.EAST;
+		gbc_lblNomeDoArquivo.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNomeDoArquivo.gridx = 0;
+		gbc_lblNomeDoArquivo.gridy = 5;
+		contentPane.add(lblNomeDoArquivo, gbc_lblNomeDoArquivo);
+
+		txtNomeArquivo = new JTextField();
+		txtNomeArquivo.setColumns(10);
+		GridBagConstraints gbc_txtNomeArquivo = new GridBagConstraints();
+		gbc_txtNomeArquivo.gridwidth = 3;
+		gbc_txtNomeArquivo.insets = new Insets(0, 0, 5, 5);
+		gbc_txtNomeArquivo.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtNomeArquivo.gridx = 1;
+		gbc_txtNomeArquivo.gridy = 5;
+		contentPane.add(txtNomeArquivo, gbc_txtNomeArquivo);
+
+		btnPesquisar = new JButton("Pesquisar");
+		GridBagConstraints gbc_btnPesquisar = new GridBagConstraints();
+		gbc_btnPesquisar.insets = new Insets(0, 0, 5, 0);
+		gbc_btnPesquisar.gridx = 4;
+		gbc_btnPesquisar.gridy = 5;
+		contentPane.add(btnPesquisar, gbc_btnPesquisar);
+
+		JScrollPane scrollPane = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 0);
+		gbc_scrollPane.gridwidth = 5;
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridx = 0;
+		gbc_scrollPane.gridy = 6;
+		contentPane.add(scrollPane, gbc_scrollPane);
+
+		table = new JTable();
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent evt) {
+
+				if (evt.getClickCount() == 2) {
+					// IP
+					Object IP = table.getValueAt(table.getSelectedRow(), 1);
+					
+					// Porta
+					Object porta = table.getValueAt(table.getSelectedRow(), 2);
+					
+					// Nome Arquivo
+					Object nomeArquivo = table.getValueAt(table.getSelectedRow(), 3);
+
+					
+					try {
+						registry = LocateRegistry.getRegistry((String) IP, (int) porta);
+
+						IServer clienteServidor = (IServer) registry.lookup(IServer.NOME_SERVICO);
+
+						clienteServidor.registrarCliente(cliente);
+						
+						Arquivo arquivo = new Arquivo();
+						arquivo.setNome((String) nomeArquivo);
+
+						byte[] baixarArquivo = clienteServidor.baixarArquivo(arquivo);
+
+						writeFile(new File("C:\\JShare\\Downloads\\" + arquivo.getNome()), baixarArquivo);
+						
+					} catch (RemoteException e) {
+						e.printStackTrace();
+					} catch (NotBoundException e) {
+						e.printStackTrace();
+					}
+
+				}
+			}
+		});
+		scrollPane.setViewportView(table);
+
+		btnConectar = new JButton("Conectar");
+		GridBagConstraints gbc_btnConectar = new GridBagConstraints();
+		gbc_btnConectar.anchor = GridBagConstraints.EAST;
+		gbc_btnConectar.insets = new Insets(0, 0, 0, 5);
+		gbc_btnConectar.gridx = 3;
+		gbc_btnConectar.gridy = 7;
+		contentPane.add(btnConectar, gbc_btnConectar);
+
+		btnDesconectar = new JButton("Desconectar");
+		GridBagConstraints gbc_btnDesconectar = new GridBagConstraints();
+		gbc_btnDesconectar.gridx = 4;
+		gbc_btnDesconectar.gridy = 7;
+		contentPane.add(btnDesconectar, gbc_btnDesconectar);
+	}
+
+	private IServer servidor;
+	private Cliente cliente;
+	private Registry registry;
+	private String meunome;
+	private JButton btnConectar;
+	private JButton btnDesconectar;
+	private JTextField txtNome;
+	private JTextField txtIP;
+	private JTextField txtPorta;
+	private JLabel lblSeuIp;
+	private JLabel label_1;
+	private JTextField txtMyIP;
+	private JTextField txtMyPort;
+	private JLabel lblNomeDoArquivo;
+	private JButton btnPesquisar;
+	private JTextField txtNomeArquivo;
+
+	private ModelArquivo modelo;
+	private JTable table;
+
+	public void configurar() {
+
+		btnConectar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				conectar();
+			}
+		});
+
+		btnDesconectar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				desconectar();
+			}
+		});
+
+		btnPesquisar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				consultarArquivos();
+			}
+		});
+
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				desconectar();
+			}
+		});
+	}
+
+	protected void consultarArquivos() {
+		try {
+			Map<Cliente, List<Arquivo>> arquivosListados = servidor.procurarArquivo(txtNomeArquivo.getText());
+
+			modelo = new ModelArquivo(arquivosListados);
+
+			table.setModel(modelo);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Conecta no servidor.
+	 */
+	protected void conectar() {
+
+		meunome = txtNome.getText().trim();
+		if (meunome.length() == 0) {
+			JOptionPane.showMessageDialog(this, "Você precisa digitar um nome!");
+			return;
+		}
+
+		// Endereço IP
+		String host = txtIP.getText().trim();
+		if (!host.matches("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}")) {
+			JOptionPane.showMessageDialog(this, "O endereço ip parece inválido!");
+			return;
+		}
+
+		// Porta
+		String strPorta = txtPorta.getText().trim();
+		if (!strPorta.matches("[0-9]+") || strPorta.length() > 5) {
+			JOptionPane.showMessageDialog(this, "A porta deve ser um valor numérico de no máximo 5 dígitos!");
+			return;
+		}
+		int intPorta = Integer.parseInt(strPorta);
+
+		// Iniciando objetos para conexão.
+		try {
+			registry = LocateRegistry.getRegistry(host, intPorta);
+
+			servidor = (IServer) registry.lookup(IServer.NOME_SERVICO);
+			// cliente = (Cliente) UnicastRemoteObject.exportObject(this, 0);
+
+			cliente = new Cliente();
+			cliente.setIp(txtMyIP.getText());
+			cliente.setPorta(Integer.parseInt(txtMyPort.getText()));
+			cliente.setNome(txtNome.getText());
+
+			// Avisando o servidor que está entrando no Chat.
+			servidor.registrarCliente(cliente);
+
+			// Publicar minha lista de arquivos...
+			List<Arquivo> lista = getMyListArchives();
+
+			servidor.publicarListaArquivos(cliente, lista);
+
+			// Aqui tenho que ficar pronto para receber uma conexão..
+			iniciarServico();
+			
+			
+			
+			btnDesconectar.setEnabled(true);
+			btnConectar.setEnabled(false);
+			txtNome.setEnabled(false);
+			txtIP.setEnabled(false);
+			txtPorta.setEnabled(false);
+			txtMyIP.setEnabled(false);
+			txtMyPort.setEnabled(false);
+
+			btnConectar.setEnabled(false);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private void iniciarServico() {
+		
+			String strPorta = txtMyPort.getText().trim();
+
+			if (!strPorta.matches("[0-9]+") || strPorta.length() > 5) {
+				JOptionPane.showMessageDialog(this, "A porta deve ser um valor numérico de no máximo 5 dígitos!");
+				return;
+			}
+			int intPorta = Integer.parseInt(strPorta);
+			if (intPorta < 1024 || intPorta > 65535) {
+				JOptionPane.showMessageDialog(this, "A porta deve estar entre 1024 e 65535");
+				return;
+			}
+
+			try {
+		
+				IServer Eu = (IServer) UnicastRemoteObject.exportObject(this, 0);
+				registry = LocateRegistry.createRegistry(intPorta);
+				
+				registry.rebind(IServer.NOME_SERVICO, Eu);
+
+			} catch (RemoteException e) {
+				JOptionPane.showMessageDialog(this, "Erro criando registro, verifique se a porta já não está sendo usada.");
+				e.printStackTrace();
+			}
+
+	}
+
+	private List<Arquivo> getMyListArchives() {
+		File dirStart = new File("C:\\JShare\\Uploads\\");
+
+		List<Arquivo> listaArquivos = new ArrayList<>();
+
+		for (File file : dirStart.listFiles()) {
+			if (file.isFile()) {
+				Arquivo arquivo = new Arquivo();
+				arquivo.setNome(file.getName());
+				arquivo.setTamanho(file.length());
+				listaArquivos.add(arquivo);
+			}
+		}
+
+		/*
+		 * System.out.println("Arquivos"); for (Arquivo arq : listaArquivos) { System.out.println("\t" + arq.getTamanho() + "\t" + arq.getNome()); }
+		 */
+
+		return listaArquivos;
+	}
+
+	/**
+	 * Desconecta do servidor.
+	 */
+	protected void desconectar() {
+		try {
+
+			if (servidor != null) {
+				servidor.desconectar(cliente);
+				// UnicastRemoteObject.unexportObject(this, true); Por algum
+				// motivo nao ta funfando..
+
+				servidor = null;
+			}
+
+			// mostrar("Você saiu do chat.");
+
+			btnDesconectar.setEnabled(false);
+
+			btnConectar.setEnabled(true);
+			txtNome.setEnabled(true);
+			txtIP.setEnabled(true);
+			txtPorta.setEnabled(true);
+			txtMyIP.setEnabled(true);
+			txtMyPort.setEnabled(true);
+
+			btnConectar.setEnabled(true);
+
+			registry = null;
+			servidor = null;
+
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected byte[] readFile(File arq) {
+		Path path = Paths.get(arq.getPath());
+		try {
+			byte[] dados = Files.readAllBytes(path);
+			return dados;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	protected void writeFile(File arq, byte[] dados) {
+		try {
+			Files.write(Paths.get(arq.getPath()), dados, StandardOpenOption.CREATE);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+	
+	
+	@Override
+	public void registrarCliente(Cliente c) throws RemoteException {
+		
+	}
+
+	@Override
+	public void publicarListaArquivos(Cliente c, List<Arquivo> lista) throws RemoteException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Map<Cliente, List<Arquivo>> procurarArquivo(String nome) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public byte[] baixarArquivo(Arquivo arq) throws RemoteException {
+		
+		List<Arquivo> myListArchives = getMyListArchives();
+		
+		for (Arquivo arquivo : myListArchives) {
+			// Consultando aqui.. se eu achar o arquivo.. hahahaha ai deu certo meu xapa.. kk
+			if (arquivo.getNome().contains(arq.getNome())){
+				// Aqui começa o baile..
+				
+				byte[] readFile = readFile(new File("C:\\JShare\\Uploads\\" + arq.getNome()));
+				
+				return readFile;
+			}
+		}
+		
+		
+		return null;
+	}
+
+	@Override
+	public void desconectar(Cliente c) throws RemoteException {
+		// TODO Auto-generated method stub
+
+	}
 }
--- JShare135208/src/br/univel/cliente/ModelArquivo.java
+++ JShare135208/src/br/univel/cliente/ModelArquivo.java
@@ -1,5 +1,112 @@
 package br.univel.cliente;
 
-public class ModelArquivo {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.swing.table.AbstractTableModel;
+
+import br.univel.interfaces.Cliente;
+import br.univel.pojos.Arquivo;
+
+public class ModelArquivo extends AbstractTableModel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	Map<Cliente, List<Arquivo>> ArquivosListados = new HashMap<>();
+
+	private Object[][] matriz;
+
+	private int linhas;
+
+	public ModelArquivo(Map<Cliente, List<Arquivo>> lista) {
+		this.ArquivosListados = lista;
+
+		linhas = 0;
+
+		for (Entry<Cliente, List<Arquivo>> e : ArquivosListados.entrySet()) {
+			linhas += e.getValue().size();
+		}
+
+		matriz = new Object[linhas][5];
+
+		int linha = 0;
+		for (Entry<Cliente, List<Arquivo>> e : ArquivosListados.entrySet()) {
+
+			for (Arquivo arq : e.getValue()) {
+
+				matriz[linha][0] = e.getKey().getNome();
+				matriz[linha][1] = e.getKey().getIp();
+				matriz[linha][2] = e.getKey().getPorta();
+				matriz[linha][3] = arq.getNome();
+				matriz[linha][4] = arq.getTamanho();
+
+				linha++;
+			}
+
+		}
+	}
+
+	@Override
+	public int getColumnCount() {
+		return 5;
+	}
+
+	@Override
+	public int getRowCount() {
+		return linhas;
+	}
+
+	@Override
+	public Object getValueAt(int row, int col) {
+		switch (col) {
+		// nome do arquivo
+		case 0:
+			return matriz[row][0];
+
+		case 1:
+			return matriz[row][1];
+
+		case 2:
+			return matriz[row][2];
+
+		case 3:
+			return matriz[row][3];
+
+		case 4:
+			return matriz[row][4];
+
+		default:
+			return "";
+		}
+	}
+
+	@Override
+	public String getColumnName(int column) {
+		switch (column) {
+		// nome do arquivo
+		case 0:
+			return "Nome do Cliente";
+
+		case 1:
+			return "Ip do Cliente";
+
+		case 2:
+			return "Porta do Cliente";
+
+		case 3:
+			return "Nome do Arquivo";
+
+		case 4:
+			return "Tamanho do Arquivo";
+
+		default:
+			return "";
+		}
+	}
 
 }
